e6d137f4dbe3a4fb48c892364ce3821fe01bb4b0
ttajakka <tuomas.tajakka@gmail.com>
2023-08-02 23:22:42 +0300

    add step 'Deploy app' to deployment pipeline

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(cors())
+
+morgan.token('req-body', (req) => {
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :req-body'))
 
 app.use(express.static('build'))
 
@@ -12,7 +19,7 @@ app.get('/health', (req, res) => {
 })
 
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT  || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
