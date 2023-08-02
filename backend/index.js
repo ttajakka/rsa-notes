ee77c7388c705076584e212d08bbc8924c4d9fc7
ttajakka <tuomas.tajakka@gmail.com>
2023-08-02 22:33:27 +0300

    create initial deployment pipeline

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.use(express.static('build'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT  || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
