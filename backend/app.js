6a3dfc0dd4f07f1bd788ebd505f0abdfa40237c6
ttajakka <tuomas.tajakka@gmail.com>
2023-08-15 15:50:02 +0300

    add get endpoints for users and messages to backend

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,26 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 
+const users = [
+  {
+    id: 0,
+    username: 'test_user',
+    pubKey: (BigInt(15732772019) * BigInt(10270832711)).toString(),
+  },
+]
+
+const messages = []
+
 app.use(cors())
 
 morgan.token('req-body', (req) => {
   return JSON.stringify(req.body)
 })
-app.use(morgan(':method :url :status :req[content-length] - :response-time ms :req-body'))
+app.use(
+  morgan(
+    ':method :url :status :req[content-length] - :response-time ms :req-body'
+  )
+)
 
 app.use(express.static('build'))
 
@@ -16,4 +30,27 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', (req, res) => {
+  const body = req.body
+
+  const newUser = {
+    id: users.length,
+    username: body.username,
+    pubKey: body.pubKey
+  }
+  
+  users.push(newUser)
+
+  res.json(newUser)
+})
+
+app.get('/messages', (req, res) => {
+  res.json(messages)
+})
+
 module.exports = app
+9
